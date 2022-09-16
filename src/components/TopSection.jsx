02f46b68865b32886eaebf80b506249dd106afcf
Tye Rooth <tyerooth@gmail.com>
2022-09-16 16:49:18 -0400

    rough top section for desktop screens created.

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,17 +1,18 @@
 import './TopSection.scss';
+import '../App.css';
 
 import headshot from '../assets/tye-800w.jpg';
 
 const TopSection = () => {
     return (
         <section className='top'>
-            <div className='top-bg'></div>
+            <div className='top-bg shadow'></div>
             <div className='top-info'>
                 <div className='image-container'>
                     <h1 className='full-name'>Tye Rooth</h1>
                     <img src={headshot} alt='tye' className='headshot'></img>
                 </div>
-                <div className='about-section'>
+                <div className='about-section shadow'>
                     <h2 className='about-heading'>About Me</h2>
                     <p className='about-content'>{about}</p>
                 </div>
